9ef78b70f17a55e3885d84b016c4fa4396f697b3
Andrea Gardoni <blazeag@gmail.com>
2016-08-07 09:13:55 +0200

    Update mineblaster.cookies.js

--- js/mineblaster.cookies.js
+++ js/mineblaster.cookies.js
@@ -15,52 +15,50 @@ mineblaster.cookies.load = function ()
 	mineblaster.field.cols_number = 10;
 	mineblaster.field.mine_number = 10;
 	
-    ca = document.cookie.split(';');
-    
-    for(i = 0; i < ca.length; i++)
-    {
-        var c = ca[i];
-        
-        while (c.charAt(0) == ' ')
-        {
-            c = c.substring(1);
-        }
-        
-        if (c.indexOf('rows_number') == 0)
-        {
-        	mineblaster.field.rows_number = c.substring(12, c.length);
-        }
-        
-        if (c.indexOf("cols_number") == 0)
-        {
-        	mineblaster.field.cols_number = c.substring(12, c.length);
-        }
-        
-        if (c.indexOf("mine_number") == 0)
-        {
-        	mineblaster.field.mine_number = c.substring(12, c.length);
-        }
-        
-        if (c.indexOf("vibration") == 0)
-        {
-        	mineblaster.vibration = (c.substring(10, c.length) === 'true') ? true : false;
-        }
-        
-        if (c.indexOf("animations") == 0)
-        {
-        	mineblaster.animations = (c.substring(11, c.length) === 'true') ? true : false;
-        }
-    }
-    
-    
-    // Set values in parameters textfields
+	ca = document.cookie.split(';');
+	
+	for (i = 0; i < ca.length; i++)
+	{
+		var c = ca[i];
+	
+		while (c.charAt(0) == ' ')
+		{
+			c = c.substring(1);
+		}
+	
+		if (c.indexOf('rows_number') == 0)
+		{
+			mineblaster.field.rows_number = c.substring(12, c.length);
+		}
+		
+		if (c.indexOf("cols_number") == 0)
+		{
+			mineblaster.field.cols_number = c.substring(12, c.length);
+		}
+		
+		if (c.indexOf("mine_number") == 0)
+		{
+			mineblaster.field.mine_number = c.substring(12, c.length);
+		}
+		
+		if (c.indexOf("vibration") == 0)
+		{
+			mineblaster.vibration = (c.substring(10, c.length) === 'true') ? true : false;
+		}
+		
+		if (c.indexOf("animations") == 0)
+		{
+			mineblaster.animations = (c.substring(11, c.length) === 'true') ? true : false;
+		}
+	}
+	
+	
+	// Set values in parameters textfields
 	$('#rows_number').val(mineblaster.field.rows_number);
 	$('#cols_number').val(mineblaster.field.cols_number);
 	$('#mine_number').val(mineblaster.field.mine_number);
 	$('#vibration').prop('checked', mineblaster.vibration);
 	$('#animations').prop('checked', mineblaster.animations);
-	
-	//document.cookie = "username=John Doe; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/";
 }
 
 
